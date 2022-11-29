21cb069f7f4280c4d4b407b72c1bb24b2431d6d0
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2022-11-29 23:09:39 +0600

    Init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="categories-container">
+      <div className="category">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body-container">
+          <h1>Hats</h1>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
